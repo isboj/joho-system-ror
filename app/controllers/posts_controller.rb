bdeeb43e542495112b493421bf03d3e6a7698bc6
Kenta Goto <v17e3007@oita-u.ac.jp>
2018-06-18 02:51:33 +0000

    lesson 08

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,29 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        #render plain: params[:post].inspect
+        # save
+        #@post = Post.new(params[:post])
+        #@post = Post.new(params[:post].permit(:title, :body))
+        @post = Post.new(post_params)
+        if @post.save
+            # redirect
+            redirect_to posts_path
+        else
+            #render plain: @post.errors.inspect
+            render 'new'
+        end
+    end
+    
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
+    
 end
